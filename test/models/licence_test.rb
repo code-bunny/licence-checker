8686fa7b9c976cbcbdbcf49130920d1684fa36ba
David Roy <david.roy@cabbit.co.uk>
2017-12-17 22:22:35 +0000

    we only have one primary licence, the system supports there being expired licences

--- test/models/licence_test.rb
+++ test/models/licence_test.rb
@@ -2,10 +2,6 @@
 require_relative '../test_helper'
 
 class LicenceTest < TestCase
-  def test_a_user_can_only_have_one_primary_licence
-    assert true
-  end
-
   def test_a_licence_has_a_product
     licence = Licence.find_by(key: 'som-sup-er-secret')
     assert_equal 'gamemaster-licence', licence.product.description
