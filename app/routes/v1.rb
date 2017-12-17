0bacb29d45c7772833b9887f77b2e707d5c85304
David Roy <david.roy@cabbit.co.uk>
2017-12-17 21:39:56 +0000

    removes some boilerplate code that is no required for this app

--- app/routes/v1.rb
+++ app/routes/v1.rb
@@ -12,27 +12,6 @@ module Routes
         def serialize(model, options = {})
           JSONAPI::Serializer.serialize(model, options)
         end
-
-        def serialize_errors(errors)
-          JSONAPI::Serializer.serialize_errors(errors)
-        end
-      end
-
-      rescue_from ActiveRecord::RecordNotFound do |e|
-        error!(serialize_errors([{ detail: e.message }]), 404, 'Content-Type' => 'text/error')
-      end
-
-      rescue_from ActiveRecord::RecordInvalid do |e|
-        error!(serialize_errors(e.record.errors), 422, 'Content-Type' => 'text/error')
-      end
-
-      rescue_from Grape::Exceptions::ValidationErrors do |e|
-        error!(serialize_errors(e), 400, 'Content-Type' => 'text/error')
-      end
-
-      before do
-        header['Access-Control-Allow-Origin'] = '*'
-        header['Access-Control-Request-Method'] = '*'
       end
 
       mount Routes::V1::Products
